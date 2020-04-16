35cac8ea5630b88f2a404f49d8c0812b0aed7e15
Ravi Shankar Kumar (DT) <ravishankak@birlasoft.com>
2020-04-16 11:09:21 +0530

    test

--- test.js
+++ test.js
@@ -1,24 +1,24 @@
-const Agenda = require('agenda');
-const { MongoClient } = require('mongodb');
+// const Agenda = require('agenda');
+// const { MongoClient } = require('mongodb');
 
-async function run() {
-  const db = await MongoClient.connect('mongodb://localhost:27017/');
-  const agenda = new Agenda().mongo(db.db('pncrfx'), 'jobs');
+// async function run() {
+//   const db = await MongoClient.connect('mongodb://localhost:27017/');
+//   const agenda = new Agenda().mongo(db.db('pncrfx'), 'jobs');
 
-  agenda.define('hello', () => {
-    console.log('Hello, World!');
-    process.exit(0);
-  });
-
-
-  await new Promise(resolve => agenda.once('ready', resolve));
+//   agenda.define('hello', () => {
+//     console.log('Hello, World!');
+//     process.exit(0);
+//   });
 
 
-  agenda.schedule('in 30 seconds', 'hello');
-  agenda.start();
-}
+//   await new Promise(resolve => agenda.once('ready', resolve));
 
-run().catch(error => {
-  console.error(error);
-  process.exit(-1);
-});
+
+//   agenda.schedule('in 30 seconds', 'hello');
+//   agenda.start();
+// }
+
+// run().catch(error => {
+//   console.error(error);
+//   process.exit(-1);
+// });
